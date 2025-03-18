2be79f9cbb169cff3989b4a302feb0cb2d8fecee
Quoc Dat <datisnoob@gmail.com>
2025-03-18 22:54:56 +0700

    Initial commit

---
+++ Models/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using CineTicket.Models;
+using Microsoft.EntityFrameworkCore;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options) { }
+
+    public DbSet<Movie> Movies { get; set; }
+    public DbSet<Users> Users { get; set; }
+    public DbSet<Tickets> Tickets { get; set; }
+    public DbSet<Showtimes> Showtimes { get; set; }
+    public DbSet<Poster> Posters { get; set; }
+}
---
+++ Models/Movie.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CineTicket.Models
+{
+    public class Movie
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Title { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        [Required]
+        public string Genre { get; set; }
+
+        [Required]
+        public DateTime ReleaseDate { get; set; }
+
+        public string PosterUrl { get; set; }
+    }
+}
